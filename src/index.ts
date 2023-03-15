2cad5dd8a56d3f1e46db06345f884d41b566ae90
weiyie <912881342@qq.com>
2023-03-15 23:28:14 +0800

    publish auto commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./base/index";
 export * from "./serverless/index";
 export * from "./time/index";
+export * from "./share/index";
---
+++ src/share/getText.ts
@@ -0,0 +1,16 @@
+export function getText(originText: string, price: number[], scale = 1): string {
+  const textArr = originText.trim().split("\n");
+
+  const priceTextPrefix = "【单价】";
+  const priceText =
+    priceTextPrefix +
+    textArr[1]
+      .replace(priceTextPrefix, "")
+      .split("；")
+      .map((item, index) => item + "￥" + Math.ceil(price[index] * scale))
+      .join("；");
+
+  textArr[1] = priceText;
+
+  return textArr.join("\n");
+}
